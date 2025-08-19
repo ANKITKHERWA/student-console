f9ed474457a4c55b50d7a6701a89239ce480d843
ankitkherwa <ankitswami024@gmail.com>
2025-08-19 11:51:45 +0530

    some change

--- src/components/pages/partner/Partner.tsx
+++ src/components/pages/partner/Partner.tsx
@@ -1,12 +1,22 @@
+'use client'
 import DotColor from '@/components/common/DotColor'
 import Tab from '@/components/common/Tab'
 import TopCommon from '@/components/common/TopCommon'
-import { CriticalTask } from '@/components/helper/Icon2'
 import React from 'react'
 import Registeredtab from './Registeredtab'
 import Prospectivetab from './Prospectivetab'
-
+import {
+  Icon10thimg,
+  Iconprospectivetab,
+  Iconregisterdtab,
+} from '@/components/helper/Icon1'
+import { useState } from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Button } from '@/components/ui/button'
 function Partner() {
+  const [activeTab, setActiveTab] = useState('Registered')
+  console.log(activeTab)
+
   return (
     <>
       <TopCommon title="Partner" />
@@ -16,21 +26,29 @@ function Partner() {
             id: 'Registered',
             value: 'Registered',
             title: 'Registered',
-            icon: <CriticalTask />,
+            icon: <Iconregisterdtab />,
             content: <Registeredtab />,
           },
           {
             id: 'Prospective',
             value: 'Prospective',
             title: 'Prospective',
-            icon: <CriticalTask />,
+            icon: <Iconprospectivetab />,
             content: <Prospectivetab />,
           },
         ]}
+        value={activeTab}
+        onChange={setActiveTab}
         rightSection={
           <>
-            <DotColor title="Critical" className="bg-[#FF0000]" />
-            <DotColor title="Growth" className="bg-[#F5640A]" />
+            {activeTab === 'Registered' ? (
+              <>
+                <DotColor title="Critical" className="bg-[#FF0000]" />
+                <DotColor title="Growth" className="bg-[#F5640A]" />
+              </>
+            ) : activeTab === 'Prospective' ? (
+              <button className="btn">Show Button</button>
+            ) : null}
           </>
         }
       />
